81535b0f8acf638e241d2dea4e01297110cd52a3
amandabmach <amandabmachh@gmail.com>
2023-03-04 02:29:04 -0300

    projeto finalizado

---
+++ src/main/java/com/example/demo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.demo.resources.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.demo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, jakarta.servlet.http.HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não Encontrado",e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+
+}
